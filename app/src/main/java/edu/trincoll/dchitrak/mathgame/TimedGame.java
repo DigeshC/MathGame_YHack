c95e58db165e3ce4727916027dee66354dae598b
DigeshC <chitrakar.digesh@gmail.com>
2019-10-26 03:47:07 -0400

    added countdown clock

--- app/src/main/java/edu/trincoll/dchitrak/mathgame/TimedGame.java
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/TimedGame.java
@@ -2,14 +2,47 @@ package edu.trincoll.dchitrak.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class TimedGame extends AppCompatActivity {
 
+    protected int counter;
+    protected final int MAXTIME = 5000;    // max time in millis
+    protected final int SECOND = 1000;      // sec is 1000 millis
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timed_game);
+
+        /* init vars for timer display */
+        counter = 0;
+        final TextView timerText = findViewById(R.id.countdownText);
+
+        // display a timer
+        new CountDownTimer(MAXTIME, SECOND){
+
+            @Override
+            public void onTick(long millisLeft) {
+                timerText.setText(String.valueOf(MAXTIME/SECOND- counter));  /* display a countdown timer in secs */
+                counter++;
+            }
+
+            @Override
+            public void onFinish(){
+                timerText.setText("Done!");
+                Intent startint = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(startint);
+            }
+
+
+
+        }.start();
+
     }
 }
