dcbb12c648bd38febbc64a3831d0df0ced60437e
DigeshC <chitrakar.digesh@gmail.com>
2019-10-26 02:52:15 -0400

    Readded Time game activity

---
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/TimedGame.java
@@ -0,0 +1,14 @@
+package edu.trincoll.dchitrak.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimedGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timed_game);
+    }
+}
