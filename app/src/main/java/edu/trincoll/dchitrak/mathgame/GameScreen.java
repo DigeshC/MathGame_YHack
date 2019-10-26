2076db2ee2ca6bfe1d87093d232093cbd95ba9b9
DigeshC <chitrakar.digesh@gmail.com>
2019-10-26 01:48:43 -0400

    Working prototype with addition and 4 random problems

---
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/GameScreen.java
@@ -0,0 +1,82 @@
+package edu.trincoll.dchitrak.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.chrono.MinguoChronology;
+import java.util.Random;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GameScreen extends AppCompatActivity {
+    Random rand = new Random();
+    int number = 1;
+    int realAnswer;
+
+    private int generateNum(int max){
+        return rand.nextInt(max);
+    }
+
+
+    private void displayProblem(){
+
+        int num1;
+        int num2;
+        num1 = generateNum(10);
+        num2 = generateNum(10);
+        TextView tv1 = (TextView) findViewById(R.id.textView);
+        TextView tv2 = (TextView) findViewById(R.id.textView2);
+
+        tv1.setText(num1+ "");
+        tv2.setText(num2+ "");
+
+        realAnswer = num1+num2;
+        checkEnd();
+    }
+
+    private void buttonClick(){
+        final EditText ed = (EditText) findViewById(R.id.editText4);
+        Button press = (Button) findViewById(R.id.button2);
+        press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = ed.getText().toString();
+                int finalValue = Integer.parseInt(value);
+                if (finalValue == realAnswer) {
+                    Log.d("Success", "Happy");
+                    displayProblem();
+                    number++;
+                } else {
+                    Log.d("Fail", "Sad");
+                }
+                ed.setText("");
+            }
+        });
+    }
+
+    private void checkEnd(){
+        Log.d("Exit", "Help");
+        if (number>3){
+            Intent startint = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(startint);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+        displayProblem();
+        buttonClick();
+    }
+}
---
+++ app/src/main/java/edu/trincoll/dchitrak/mathgame/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.trincoll.dchitrak.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button press = (Button) findViewById(R.id.button);
+        press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startint = new Intent(getApplicationContext(), GameScreen.class);
+                //startint.putExtra("com.example.twobutton.SOMETHING", "");
+                startActivity(startint);
+            }
+        });
+
+
+    }
+}
